a4e5f2be9dd211e09d67dc17b1a3baf2aae33cca
TomasAnjos2021 <202100301@estudantes.ips.pt>
2024-03-13 10:32:33 +0000

    Nivel 2

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner; 
 /**
  * Escreva uma descrição da classe WordGuessingGame aqui.
  * 
@@ -10,7 +10,7 @@ public class WordGuessingGame
   private String hiddenWord;
   private String guessedWord;
   private int numberOfTries;
-
+  private InputReader reader;
     /**
      * Construtor para objetos da classe WordGuessingGame
      */
@@ -18,13 +18,41 @@ public class WordGuessingGame
     {
         hiddenWord = "abc";
         guessedWord = "___";
-        
-        
+        reader = new InputReader();
+        this.numberOfTries = numberOfTries;
   
     }
     
-   public String showGuessWord(){
-    return guessedWord;
+    public void showWelcome(){
+     System.out.println("Bem vindo ao Word Guesssing Game");
+    }
+    
+    public boolean guess(char letter){
+    
+    for(int i=0;i<hiddenWord.length();i++){
+     if( hiddenWord.charAt(i) == letter && guessedWord.charAt(i) == '_'){
+         guessedWord = guessedWord.substring(0, i) + letter + guessedWord.substring(i + 1);   
+        }
+    }
+  
+    return guessedWord.equals(hiddenWord);
+
+    }
+    
+    public void play(){
+        char letter ;
+        do{
+         letter = reader.getChar("Coloque uma letra: ");
+    }
+    while(!guess(letter));
+    showResult();
+}
+    public void showResult(){
+        System.out.println("Parabens adivinhas-te a palavra " + hiddenWord + "em " + numberOfTries + "tentativas");
+    }
+    
+     public String showGuessWord(){
+     return guessedWord;
     }
     
     public String getHiddenWord() {
